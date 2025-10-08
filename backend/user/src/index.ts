7727d6190ccab9241b1c4a12e38184045f846d27
Hardikjoshi-18 <hardikaimnda@gmail.com>
2025-10-08 22:17:50 +0530

    third commit

--- backend/user/src/index.ts
+++ backend/user/src/index.ts
@@ -1,11 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDb from "./config/db.js";
+import {createClient} from "redis"
+import userRoutes from "./routes/user.js"
 
 dotenv.config();
 connectDb();
 
+export const redisClient = createClient({
+  url: process.env.REDIS_URL || "redis://localhost:6379",
+});
+
+redisClient
+  .connect()
+  .then(() => console.log("connected to redis"))
+  .catch(console.error);
+
 const app = express();
+
+app.use("api/v1", userRoutes)
 
 const port = process.env.PORT;
 
