3552db6a08737e4097715e11aef1e2f2093b4b2e
Hardikjoshi-18 <hardikaimnda@gmail.com>
2025-10-09 22:53:13 +0530

    fourt

--- backend/user/src/index.ts
+++ backend/user/src/index.ts
@@ -1,11 +1,18 @@
+
+
 import express from "express";
 import dotenv from "dotenv";
 import connectDb from "./config/db.js";
-import {createClient} from "redis"
-import userRoutes from "./routes/user.js"
+import { createClient } from "redis";
+import userRoutes from "./routes/user.js";
+import { connectRabbitMQ } from "./config/rabbitmq.js";
+import cors from "cors";
 
 dotenv.config();
+
 connectDb();
+
+connectRabbitMQ();
 
 export const redisClient = createClient({
   url: process.env.REDIS_URL || "redis://localhost:6379",
@@ -18,10 +25,14 @@ redisClient
 
 const app = express();
 
-app.use("api/v1", userRoutes)
+app.use(express.json());
+
+app.use(cors());
+
+app.use("/api/v1", userRoutes);
 
 const port = process.env.PORT;
 
-app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
